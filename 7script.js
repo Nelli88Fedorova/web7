a6118d7fa6ac1ecc1d85a38d345d6f7bbdf98d1e
Нелли Федорова <90643474+Nelli88Fedorova@users.noreply.github.com>
2021-11-07 13:40:47 +0300

    Add files via upload

---
+++ 7script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() 
+{
+$(".slides").slick({
+	prevArrow: "<img src='blue.jpg' class='prev' alt='1'>",
+    nextArrow: "<img src='le.jpg' class='next' alt='2'>",
+	slidesToShow: 5,
+	slidesToScroll: 3,
+	dots: true,
+responsive:[
+{
+    breakpoint: 960,
+    settings: {
+	prevArrow: "<img src='blue.jpg' class='prev' alt='1'>",
+    nextArrow: "<img src='le.jpg' class='next' alt='2'>",
+    slidesToShow: 5,
+	slidesToScroll: 3,
+	
+	dots:true ,
+    }
+},
+{    breakpoint: 768,
+    settings: {
+    slidesToShow: 2,
+	slidesToScroll: 1,
+	prevArrow: "<img src='blue.jpg' class='prev' alt='1'>",
+    nextArrow: "<img src='le.jpg' class='next' alt='2'>",
+	dots: true,
+    }
+}
+] 	
+});
+}); 
